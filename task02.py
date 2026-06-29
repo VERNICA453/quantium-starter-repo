59ca594f23a4b6e3443ae80c869c724215c84e92
vernica <vernicaraj@gmail.com>
2026-06-29 10:38:37 +0530

    Completed Task 2 - Sales transformation

---
+++ task02.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+df1 = pd.read_csv("data/daily_sales_data_0.csv")
+df2 = pd.read_csv("data/daily_sales_data_1.csv")
+df3 = pd.read_csv("data/daily_sales_data_2.csv")
+
+
+df = pd.concat([df1, df2, df3], ignore_index=True)
+
+
+df = df[df["product"] == "pink morsel"]
+
+df["price"] = df["price"].str.replace("$", "", regex=False).astype(float)
+
+df["sales"] = df["quantity"] * df["price"]
+
+df = df[["sales", "date", "region"]]
+print(df.head())
+
+df.to_csv("output.csv", index=False)
